237f8f4bddf36de3be7ddeb170976c1f3283b0eb
jamiewoych <jw3943@columbia.edu>
2025-02-25 11:19:43 -0500

    Setting up mini project

---
+++ README.md
@@ -0,0 +1,9 @@
+# mini-project
+Spreadsheet parsing tool for manipulating metadata of salamander inventory
+### Installation
+
+conda install [list dependencies here...] -c conda-forge
+
+git clone https://github.com/jamiewoych/mini-project
+cd ./mini-project
+pip install -e
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+Call `pip install -e .` to install package locally for testing.
+"""
+
+from setuptools import setup
+
+# build command
+setup(
+    name="mini-project",
+    version="0.0.1",
+    author="Jamie Woych",
+    author_email="jw3943@columbia.edu",
+    license="GPLv3",
+    description="A package for salamander inventory",
+    classifiers=["Programming Language :: Python :: 3"],
+    entry_points={
+        "console_scripts": ["mini-project = mini-project.__main__:main"]
+    },
+)
+
+
