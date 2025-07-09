5a5fcc7d92eb841c71d286794d979ea6dc1060d9
jamiewoych <jw3943@columbia.edu>
2025-07-09 16:08:12 -0400

    change directory to real inventory

--- pleuro_parser/module.py
+++ pleuro_parser/module.py
@@ -940,7 +940,7 @@ class Rack:
             repo_root = repo.working_tree_dir
             
             # Write the current inventory DataFrame to CSV in the repo
-            file_path = os.path.join(repo_root, "pleuro_parser/salamander_inventory.csv")
+            file_path = os.path.join(repo_root, "salamander_inventory.csv")
             self.inventory.to_csv(file_path, index=False)
             
             # Stage the changes
